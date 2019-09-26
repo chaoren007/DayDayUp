d077e3ef9d2f54b2959a93b00843e1348ac3f594
LiquorSea <sea.zhong@allchips.com>
2019-09-26 18:58:33 +0800

    添加springBoot的Maven的打包plugins

--- zcBoot/src/main/java/com/zc/Main.java
+++ zcBoot/src/main/java/com/zc/Main.java
@@ -3,11 +3,9 @@ package com.zc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Main.class);
-        application.run(args);
-//        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
