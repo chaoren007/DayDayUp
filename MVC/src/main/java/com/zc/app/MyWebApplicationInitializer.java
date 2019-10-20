c2decfc2603646a54316d4702f57e54a165b5451
liquor <liquorsea@163.com>
2019-10-20 22:41:17 +0800

    1

---
+++ MVC/src/main/java/com/zc/app/MyWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.zc.app;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletException;
+
+/**
+ * @Description TODO
+ * @Author LiquorSea
+ * @Date 2019/10/20 22:07
+ **/
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+
+
+    }
+}
+  
+  
+   
