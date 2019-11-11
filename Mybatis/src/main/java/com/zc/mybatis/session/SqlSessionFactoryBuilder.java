f5d705d58418dec92b916056182a9f0916982f7b
LiquorSea <sea.zhong@allchips.com>
2019-11-11 20:22:26 +0800

    2019-11-11

--- Mybatis/src/main/java/com/zc/mybatis/session/SqlSessionFactoryBuilder.java
+++ Mybatis/src/main/java/com/zc/mybatis/session/SqlSessionFactoryBuilder.java
@@ -1,55 +1,60 @@
 /**
- * 
+ *
  */
 package com.zc.mybatis.session;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 import com.zc.mybatis.session.defaults.DefaultSqlSessionFactory;
 
 
 /**
  * SqlSessionFactoryBuilder.java
- * 
+ *
  * @author PLF
  * @date 2019年3月7日
  */
-public class SqlSessionFactoryBuilder
-{
+public class SqlSessionFactoryBuilder {
 
     /**
      * build
-     * 
+     *
      * @param fileName
-     * @return 
-     * @see 
+     * @return
+     * @see
      */
-    public SqlSessionFactory build(String fileName)
-    {
+    public SqlSessionFactory build(String fileName) {
 
         InputStream inputStream = SqlSessionFactoryBuilder.class.getClassLoader().getResourceAsStream(fileName);
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//        try {
+//            while (true) {
+//                String s = reader.readLine();
+//                System.out.println(s);
+//            }
+//        } catch (Exception e) {
+//
+//        }
         return build(inputStream);
     }
 
     /**
      * build
-     * 
+     *
      * @param inputStream
-     * @return 
-     * @see 
+     * @return
+     * @see
      */
-    public SqlSessionFactory build(InputStream inputStream)
-    {
-        try
-        {
+    public SqlSessionFactory build(InputStream inputStream) {
+        try {
             Configuration.PROPS.load(inputStream);
 
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return new DefaultSqlSessionFactory(new Configuration());
--- Mybatis/src/main/java/com/zc/mybatis/session/defaults/DefaultSqlSessionFactory.java
+++ Mybatis/src/main/java/com/zc/mybatis/session/defaults/DefaultSqlSessionFactory.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.zc.mybatis.session.defaults;
 
@@ -17,12 +17,11 @@ import com.zc.mybatis.utils.XmlUtil;
 
 /**
  * 默认SQL会话工厂实现类
- * 
+ *
  * @author PLF
  * @date 2019年3月7日
  */
-public class DefaultSqlSessionFactory implements SqlSessionFactory
-{
+public class DefaultSqlSessionFactory implements SqlSessionFactory {
 
     /** the configuration */
     private final Configuration configuration;
@@ -30,8 +29,7 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory
     /**
      * @param configuration
      */
-    public DefaultSqlSessionFactory(Configuration configuration)
-    {
+    public DefaultSqlSessionFactory(Configuration configuration) {
 
         this.configuration = configuration;
         loadMappersInfo(Configuration.getProperty(Constant.MAPPER_LOCATION).replaceAll("\\.", "/"));
@@ -43,8 +41,7 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory
      * @return
      */
     @Override
-    public SqlSession openSession()
-    {
+    public SqlSession openSession() {
 
         SqlSession session = new DefaultSqlSession(this.configuration);
 
@@ -53,35 +50,28 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory
 
     /**
      * loadMappersInfo
-     * 
+     *
      * @param dirName
      * @see
      */
-    private void loadMappersInfo(String dirName)
-    {
+    private void loadMappersInfo(String dirName) {
 
         URL resources = DefaultSqlSessionFactory.class.getClassLoader().getResource(dirName);
 
         File mappersDir = new File(resources.getFile());
 
-        if (mappersDir.isDirectory())
-        {
+        if (mappersDir.isDirectory()) {
 
             // 显示包下所有文件
             File[] mappers = mappersDir.listFiles();
-            if (CommonUtis.isNotEmpty(mappers))
-            {
-                for (File file : mappers)
-                {
+            if (CommonUtis.isNotEmpty(mappers)) {
+                for (File file : mappers) {
 
                     // 对文件夹继续递归
-                    if (file.isDirectory())
-                    {
+                    if (file.isDirectory()) {
                         loadMappersInfo(dirName + "/" + file.getName());
 
-                    }
-                    else if (file.getName().endsWith(Constant.MAPPER_FILE_SUFFIX))
-                    {
+                    } else if (file.getName().endsWith(Constant.MAPPER_FILE_SUFFIX)) {
 
                         // 只对XML文件解析
                         XmlUtil.readMapperXml(file, this.configuration);
