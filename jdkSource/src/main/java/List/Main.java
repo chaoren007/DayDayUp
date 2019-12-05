f6274a46f65feed010193cdde738faa45e7e9b30
LiquorSea <sea.zhong@allchips.com>
2019-12-05 09:26:32 +0800

    --

--- jdkSource/src/main/java/List/Main.java
+++ jdkSource/src/main/java/List/Main.java
@@ -1,5 +1,7 @@
 package List;
 
+import bean.Student;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -28,28 +30,3 @@ public class Main {
     }
 }
 
-class Student {
-    private  String id;
-    private  String name;
-
-    public Student(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public  String getId() {
-        return id;
-    }
-
-    public  void setId(String id) {
-        this.id = id;
-    }
-
-    public  String getName() {
-        return name;
-    }
-
-    public  void setName(String name) {
-        this.name = name;
-    }
-}
